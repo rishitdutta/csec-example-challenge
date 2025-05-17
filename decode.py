bf3aa8855e0e64cc3764fcf93b53f54b922bad6e
Rishit Dutta <rishitdutta03@gmail.com>
2025-05-17 05:58:05 +0530

    updated decode.py

--- decode.py
+++ decode.py
@@ -4,10 +4,10 @@ im = Image.open("big-chonk.png")
 w,h = im.size
 bits = []
 for x in range(w):
-  for y in range(h):
+  for y in range(h):  #row-major order
     p = im.getpixel((x,y))
-    r, g, b, a = im.getpixel((x, y))     
-    bits.append(str(r & 1))         
+    r, g, b, a = im.getpixel((x, y))    #this image has the alpha(a) channel too which determines the opacity of the pixel
+    bits.append(str(r & 1))       #r is 1 byte, so doing bitwise & with 1 (which is same as 00000001) we can get the LSB of r
     bits.append(str(g & 1))          
     bits.append(str(b & 1)) 
     bits.append(str(a & 1)) 
